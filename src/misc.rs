fe01ed8bf0e0b17f1d6f63e777be8767af840a12
nathanfaucett <nathanfaucett@gmail.com>
2015-07-05 12:29:54 -0500

    updates

--- src/misc.rs
+++ src/misc.rs
@@ -93,27 +93,15 @@ fn test_determinant() {
 
 #[inline(always)]
 pub fn transpose<T: Num>(out: &mut [T; 9], a: [T; 9]) -> &mut [T; 9] {
-    if *out == a {
-        let a01 = a[1];
-        let a02 = a[2];
-        let a12 = a[5];
-        out[1] = a[3];
-        out[2] = a[6];
-        out[3] = a01;
-        out[5] = a[7];
-        out[6] = a02;
-        out[7] = a12;
-    } else {
-        out[0] = a[0];
-        out[1] = a[3];
-        out[2] = a[6];
-        out[3] = a[1];
-        out[4] = a[4];
-        out[5] = a[7];
-        out[6] = a[2];
-        out[7] = a[5];
-        out[8] = a[8];
-    }
+    out[0] = a[0];
+    out[1] = a[3];
+    out[2] = a[6];
+    out[3] = a[1];
+    out[4] = a[4];
+    out[5] = a[7];
+    out[6] = a[2];
+    out[7] = a[5];
+    out[8] = a[8];
     out
 }
 #[test]
